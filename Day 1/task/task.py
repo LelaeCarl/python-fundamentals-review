ad227d11aa5d4c00f5ece9f5151c5dc4fbc8d4d1
Sage <carllelae@gmail.com>
2025-01-14 10:28:12 +0800

    Review session 1

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Hi welcome to our band generator app")
+city = input("Which city did you grow up in?\n:")
+pet = input("what's the name of your pet?\n:")
+print("Great! Your new bandname is " + city +" " + pet + ".\nGodspeed :)")
