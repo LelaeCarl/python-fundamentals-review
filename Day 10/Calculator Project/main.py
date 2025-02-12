d4a631930e172d4b9df34faa1b97e0d1eec1bcf5
Sage <carllelae@gmail.com>
2025-02-12 14:41:03 +0800

    update

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,23 @@
 def add(n1, n2):
     return n1 + n2
 
+#three other functions
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+#use dictionary operations to perform calculations
 
---
+++ Day 10/Functions with Outputs/task.py
@@ -0,0 +1,6 @@
+def format_name(f_name, l_name):
+    finale = (f_name + " " + l_name).title()
+    return finale
+
+
+print(format_name("amena", "amapiano"))
